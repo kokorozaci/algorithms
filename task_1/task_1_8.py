9c91d959b1b8666c61595159c0c62d2cc2cd6122
Mark Anna <anna.mark.spb@gmail.com>
2020-02-14 21:00:04 +0300

    task_1.1

--- task_1/task_1_8.py
+++ task_1/task_1_8.py
@@ -1,9 +1,9 @@
 a = int(input('Введите a: '))
 b = int(input('Введите b: '))
 c = int(input('Введите c: '))
-if b < a < c or (b > a > c and c < b):
+if b < a < c or c < a < b:
     print(f'Среднее число: {a}')
-elif a < b < c or (a > b > c and a > c):
+elif a < b < c or c < b < a:
     print(f'Среднее число: {b}')
 else:
     print(f'Среднее число: {c}')
