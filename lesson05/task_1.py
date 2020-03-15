bfbd9482ec2d519009911f9bf071d69e1aea6e79
Mark Anna <anna.mark.spb@gmail.com>
2020-03-15 19:36:59 +0300

    lesson05_2

--- lesson05/task_1.py
+++ lesson05/task_1.py
@@ -15,7 +15,17 @@ for _ in range(int(input('Введите число предприятий: ')))
                  int(input('Прибыль за четвёртый квартал: ')))
 
 mean_value = sum(map(sum, dict_a.values()))/len(dict_a)
+
+print('Средняя прибыль: ', mean_value)
+val_more_mean = []
+val_smaller_mean = []
 for k, v in dict_a.items():
     if sum(v) > mean_value:
-        print(k)
+        val_more_mean.append((k, sum(v)))
+    else:
+        val_smaller_mean.append((k, sum(v)))
+print('Предприятия с прибылью выше среднего:')
+[print(f'{n[0]}. Прибыль - {n[1]}') for n in val_more_mean]
+print('Предприятия с прибылью ниже среднего:')
+[print(f'{n[0]}. Прибыль - {n[1]}') for n in val_smaller_mean]
 
